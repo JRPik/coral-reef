07406d44cb9e9595116744c861bff55bc0015de4
arowe4 <78028846+arowe4@users.noreply.github.com>
2022-04-02 13:44:07 -0400

    login page update

--- app/screens/login.js
+++ app/screens/login.js
@@ -22,132 +22,120 @@ export default function Login({ navigation }) {
   const pressedHandler = () => {
     navigation.navigate("Logon");
   };
+  
   const pressedHandler2 = () => {
     navigation.navigate("Home");
   };
+  
   let [fontsLoaded] = useFonts({
     Roboto_400Regular,
   });
-
+  
   if (!fontsLoaded) {
     return <AppLoading />;
   } else {
     return (
       <ImageBackground
-        style={styles.imageBackground}
-        source={require("../assets/images/coralBackground.jpg")}
+      style={styles.imageBackground}
+      source={require("../assets/images/coralReefBackground.jpg")}
       >
-      <SafeAreaView style={styles.container}>
-        <ScrollView>
-          <KeyboardAvoidingView style={styles.imageContainer}>
-            <Image
-              style={styles.image}
-              source={require("../assets/images/logo.jpg")}
-            />
-          </KeyboardAvoidingView>
-
-          <KeyboardAvoidingView style={{ paddingLeft: "15%" }}>
-            <TextInput 
-              style={styles.textbox} 
-              placeholder="User ID" 
-            />
-            <TextInput
+        <SafeAreaView style={styles.container}>
+          <ScrollView>
+            <KeyboardAvoidingView style={styles.logoImageCont}>
+              <Image
+              style={styles.logoImage}
+              source={require("../assets/images/logo.jpg")}/>
+            </KeyboardAvoidingView>
+            
+            <KeyboardAvoidingView style={{ paddingLeft: "15%" }}>
+              <TextInput
+              style={styles.textbox}
+              placeholder="User ID"
+              />
+              <TextInput
               secureTextEntry={true}
               style={styles.textbox}
               placeholder="Password"
-            />
-          </KeyboardAvoidingView>
-          <TouchableOpacity
-            style={styles.buttonContainer}
-            onPress={pressedHandler2}
-          >
-            <Text
-              style={{
-                textAlign: "center",
-                color: colors.backGroundOne,
-                fontFamily: "Roboto_400Regular",
-              }}
-            >
-              Login
-            </Text>
-          </TouchableOpacity>
-          <Text style={styles.text2}>Don't Have an Account?</Text>
-          <TouchableOpacity
-            style={styles.buttonContainer}
-            onPress={pressedHandler}
-          >
-            <Text
-              style={{
-                textAlign: "center",
-                color: colors.backGroundOne,
-                fontFamily: "Roboto_400Regular",
-              }}
-            >
-              Create Account
-            </Text>
-          </TouchableOpacity>
-        </ScrollView>
-        <StatusBar style="auto" />
-      </SafeAreaView>
-    </ImageBackground>
+              />
+            </KeyboardAvoidingView>
+          
+            <TouchableOpacity style={styles.buttonContainer} onPress={pressedHandler2}>
+              <Text style={styles.buttonText}>
+                Login
+              </Text>
+            </TouchableOpacity>
+          
+            <Text style={styles.haveAcctText}>Don't Have an Account?</Text>
+          
+            <TouchableOpacity style={styles.buttonContainer} onPress={pressedHandler}>
+              <Text style={styles.buttonText}>
+                Create Account
+              </Text>
+            </TouchableOpacity>
+          </ScrollView>
+          <StatusBar style="auto" />
+        </SafeAreaView>
+      </ImageBackground>
     );
-  }
+  } 
 }
 
 const styles = StyleSheet.create({
   buttonContainer: {
     backgroundColor: colors.primary,
-    width: "45%",
+    width: "30%",
     alignSelf: "center",
     borderRadius: 20,
     padding: 10,
   },
+  buttonText: {
+    textAlign: "center",
+    color: colors.backGroundOne,
+    fontWeight: "bold",
+  },
   container: {
     //flex: 1,
     //backgroundColor: colors.backGroundOne,
-    alignContent: "center",
-  },
-  image: {
-    width: 250,
-    height: 250,
-    resizeMode: "contain",
+    //alignContent: "center",
   },
   imageBackground: {
     //resizeMode: "contain",
     flex: 1,
   },
-  imageContainer: {
+  logoImage: {
+    width: 250,
+    height: 250,
+    resizeMode: "contain",
+  },
+  logoImageCont: {
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: colors.backGroundOne,
-    margin: 16,
-    borderRadius: 8,
-    shadowColor: colors.shadowTwo,
-    shadowOpacity: 0.3,
-    shadowRadius: 8,
-    shadowOffset: { height: 1, width: 0.3 },
+    margin: 10,
+    borderRadius: 50,
+    //shadowColor: colors.shadowTwo,
+    //shadowOpacity: 0.3,
+    //shadowRadius: 8,
+    //shadowOffset: { height: 1, width: 0.3 },
   },
-  text: {
-    marginTop: 70,
-    fontSize: 20,
-    textAlign: "center",
-  },
-  text2: {
+  haveAcctText: {
     marginBottom: 15,
     marginTop: 15,
     fontSize: 15,
     textAlign: "center",
-    fontFamily: "Roboto_400Regular",
+    color: colors.backGroundOne,
+    fontWeight: "bold",
   },
   textbox: {
     backgroundColor: colors.backGroundOne,
     height: 40,
-    width: "75%",
+    width: "80%",
     margin: 10,
     borderRadius: 5,
     shadowColor: colors.shadowOne,
     shadowOpacity: 0.3,
     fontFamily: "Roboto_400Regular",
     shadowOffset: { height: 1, width: 0.3 },
+    paddingLeft: 5,
   },
 });
